070375065b2a1fa1c8566ce375b3668ad04ceee7
BryanFoo07 <bryanfoo17@gmail.com>
2026-09-10 02:03:06 +0800

    fix wrong version name db

--- infra/modules/rds/variables.tf
+++ infra/modules/rds/variables.tf
@@ -35,7 +35,7 @@ variable "db_password" {
 variable "instance_class" {
   description = "RDS instance class."
   type        = string
-  default     = "db.t4.micro"
+  default     = "db.t4g.micro"
 }
 
 variable "allocated_storage" {
